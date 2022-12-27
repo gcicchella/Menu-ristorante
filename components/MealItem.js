d64806e986f7c1f827abd496b3f65958fcd2b00a
g.cicchella <giuseppecicchella28@gmail.com>
2022-12-27 16:01:53 +0100

    arricchita schermata cibo con ulteriori informazioni

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,22 +1,33 @@
 import {View, Text, Pressable, Image, StyleSheet, Platform} from "react-native";
+import {useNavigation} from "@react-navigation/native";
+import MealDetails from "./MealDetails";
 
-function MealItem({title, imageUrl, duration, complexity, affordability}){
+function MealItem({id, title, imageUrl, duration, complexity, affordability}){
+    const navigation = useNavigation();
+
+    function selectMealItemHandler(){
+        navigation.navigate('MealDetail', {
+            mealId: id
+        });
+    }
+
     return (
         <View style={styles.mealItem}>
             <Pressable
                 android_ripple={{color: '#ccc'}}
                 style={({ pressed }) =>  pressed ? styles.buttonPressed : null}
+                onPress={selectMealItemHandler}
             >
               <View style={styles.innerContainer}>
                   <View>
                       <Image source={{ uri: imageUrl }} style={styles.image}/>
                       <Text style={styles.title}> {title} </Text>
                   </View>
-                  <View style={styles.details}>
-                      <Text style={styles.detailItem}> {duration} </Text>
-                      <Text style={styles.detailItem}> {complexity.toUpperCase()} </Text>
-                      <Text style={styles.detailItem}> {affordability.toUpperCase()} </Text>
-                  </View>
+                  <MealDetails
+                      duration={duration}
+                      affordability={affordability}
+                      complexity={complexity}
+                  />
               </View>
             </Pressable>
         </View>
@@ -53,15 +64,5 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 18,
         margin: 8
-    },
-    details: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        padding: 8
-    },
-    detailItem: {
-        marginHorizontal: 4,
-        fontSize: 12
     }
 });
