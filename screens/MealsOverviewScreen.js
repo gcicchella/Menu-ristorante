ba1d4ea17d1a8745c95ae23aa5f16db9cb8ac523
g.cicchella <giuseppecicchella28@gmail.com>
2022-12-27 12:26:50 +0100

    aggiunto i pasti alla seconda schermata

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,5 +1,5 @@
 import {MEALS} from "../data/dummy-data";
-import {FlatList, StyleSheet, Text, View} from "react-native";
+import {FlatList, StyleSheet, View} from "react-native";
 import MealItem from "../components/MealItem";
 
 function MealsOverviewScreen({route}){
