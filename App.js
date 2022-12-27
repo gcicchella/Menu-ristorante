fa3c91bd3a384bf37eb1b6e2eee874b8afc34f44
g.cicchella <giuseppecicchella28@gmail.com>
2022-12-27 17:19:23 +0100

    aggiunto bottone header nella sezione dettaglio

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 import {NavigationContainer} from "@react-navigation/native";
 import {createNativeStackNavigator} from "@react-navigation/native-stack";
 
@@ -39,7 +39,10 @@ export default function App() {
                       //   };
                       // }}
                   />
-                  <Stack.Screen name="MealDetail" component={MealDetailScreen}/>
+                  <Stack.Screen
+                      name="MealDetail"
+                      component={MealDetailScreen}
+                  />
               </Stack.Navigator>
           </NavigationContainer>
       </>
@@ -48,5 +51,5 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-  },
+  }
 });
